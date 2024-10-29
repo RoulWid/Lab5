e9ef29ce088d1b1ed0f105721b8385acb035c30b
Роман <Nikandrov.Ro@yandex.ru>
2024-10-29 09:33:48 +0300

    click and saveDate script

--- script/script.js
+++ script/script.js
@@ -18,3 +18,18 @@ function addTask(){
     inputBox.value = "";
     saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
